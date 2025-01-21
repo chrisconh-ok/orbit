2ac6408df353d1b65cb5fac947404ce17e4a0041
Chris Aramayo <chris.techloop@gmail.com>
2025-01-21 16:52:39 -0300

    Update to new folder system

---
+++ src/features/blog/index.ts
@@ -0,0 +1,1 @@
+// Lógica específica (fetch, utils, etc.)
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./node_modules/flowbite/**/*.js'
+	],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				lato: ['Lato', 'Arial', 'Helvetica', 'sans-serif']
+			},
+			colors: {
+				brand: {
+					dark: '#272727', // Color oscuro
+					primary: '#50d1c5', // Color principal
+					secundary: '#ffb86c' // Color de acento
+				}
+			}
+		}
 	},
-	plugins: [],
+	plugins: [require('flowbite/plugin')]
 }
